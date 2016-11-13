badad89e9b0df1e338405c44165c1457d76de4f6
AlanPerezCo <alan.perezco@udlap.mx>
2016-11-13 17:13:22 -0600

    Project Anna's World

---
+++ Anna's World - Alfa 1.4/Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundCheck : MonoBehaviour {
+
+    private annaInput player;
+
+    void Start()
+    {
+        player = gameObject.GetComponentInParent<annaInput>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        player.grounded = true;
+
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        player.grounded = true;
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        player.grounded = false;
+    }
+}
